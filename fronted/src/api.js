bad3f5870e666cb2194a0713e0e3d65bdb9565b8
Sai Kumar Gundeboina <75746273+Sai-Kumar-Gundeboina@users.noreply.github.com>
2026-07-28 12:24:21 +0000

    update

--- fronted/src/api.js
+++ fronted/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://upgraded-lamp-4pgpg9p5xvf755-8000.app.github.dev/",
+    // baseURL: "https://upgraded-lamp-4pgpg9p5xvf755-8000.app.github.dev/",
+    baseURL: "http://13.234.4.203/",
     headers: {
         "Content-Type": "application/json",
     },
